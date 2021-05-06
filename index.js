d661ee220570448fb7e8d22d996aef23504791c4
Bera <berakoc97@gmail.com>
2021-05-06 22:01:09 +0300

    *New: Added basic API fetching structure and root html file

---
+++ index.js
@@ -0,0 +1,7 @@
+import connect from '/src/api/connect.js'
+
+const main = async () => {
+    connect()
+}
+
+main()
---
+++ src/api/connect.js
@@ -0,0 +1,8 @@
+const connect = (() => {
+    SC.initialize({
+        clientId: 'cd9be64eeb32d1741c17cb39e41d254d'
+    })
+    console.log('Connected to SoundCloud API')
+})
+
+export default connect
